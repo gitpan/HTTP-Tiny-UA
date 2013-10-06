a3ad61125b9060a809b10e600700955f853e6375
David Golden <dagolden@cpan.org>
2013-10-06 02:21:02 +0000

    Import of DAGOLDEN/HTTP-Tiny-UA-0.001 from CPAN.
    gitpan-cpan-distribution: HTTP-Tiny-UA
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         DAGOLDEN/HTTP-Tiny-UA-0.001.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

---
+++ lib/HTTP/Tiny/UA/Response.pm
@@ -0,0 +1,89 @@
+use v5.10;
+use strict;
+use warnings;
+
+package HTTP::Tiny::UA::Response;
+# ABSTRACT: Wrap HTTP::Tiny response as objects with accessors
+our $VERSION = '0.001'; # VERSION
+
+use Class::Tiny qw( success url status reason content headers protocol );
+
+
+sub header {
+    my ( $self, $field ) = @_;
+    return $self->headers->{ lc $field };
+}
+
+1;
+
+
+# vim: ts=4 sts=4 sw=4 et:
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+HTTP::Tiny::UA::Response - Wrap HTTP::Tiny response as objects with accessors
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+    my $res = HTTP::Tiny->new->get( $url );
+
+    if ( $res->success ) {
+        say "Got " . $res->header("Content-Length") . " bytes";
+    }
+
+=head1 DESCRIPTION
+
+This module wraps an L<HTTP::Tiny> response as an object to provide some
+accessors and convenience methods.
+
+=head1 ATTRIBUTES
+
+=head2 success
+
+=head2 url
+
+=head2 protocol
+
+=head2 status
+
+=head2 reason
+
+=head2 content
+
+=head2 headers
+
+=head1 METHODS
+
+=head2 header
+
+    $response->header( "Content-Length" );
+
+Return a header out of the headers hash.  The field is case-insensitive.  If
+the header was repeated, the value returned will be an array reference.
+Otherwise it will be a scalar value.
+
+=for Pod::Coverage BUILD
+
+=head1 AUTHOR
+
+David Golden <dagolden@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2013 by David Golden.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+=cut
